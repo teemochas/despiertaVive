465df928f61918a0b01d09f69ac5bb5b01d1847b
Artemio Ordaz Estrada <temochas_lost@hotmail.com>
2018-07-10 13:54:58 -0500

    despiertaVive

--- despiertaVive/despiertaVive/ViewModels/LoginViewModel.cs
+++ despiertaVive/despiertaVive/ViewModels/LoginViewModel.cs
@@ -2,11 +2,23 @@
 {
     using GalaSoft.MvvmLight.Command;
     using System;
+    using System.ComponentModel;
     using System.Windows.Input;
     using Xamarin.Forms;
 
-    public class LoginViewModel
+    public class LoginViewModel : INotifyPropertyChanged
     {
+
+        #region Events
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        #region Attributes
+        private string password;
+        private bool isRunning;
+        private bool isEnabled;
+        #endregion
+
         #region Properties
         public string Email
         {
@@ -14,16 +26,36 @@
             set;
         }
 
-        public string Password 
+        public string Password
         {
-            get;
-            set;
+            get
+            {
+                return this.password;
+            }
+            set
+            {
+                if (this.password != value)
+                {
+                    this.password = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.Password)));
+                }
+            }
         }
 
         public bool IsRunning
         {
-            get;
-            set;
+            get
+            {
+                return this.isRunning;
+            }
+            set
+            {
+                if (this.isRunning != value)
+                {
+                    this.isRunning = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.IsRunning)));
+                }
+            }
         }
 
         public bool IsRemembered
@@ -31,18 +63,35 @@
             get;
             set;
         }
+
+        public bool IsEnabled
+        {
+            get
+            {
+                return this.isEnabled;
+            }
+            set
+            {
+                if (this.isEnabled != value)
+                {
+                    this.isEnabled = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.IsEnabled)));
+                }
+            }
+        }
         #endregion
 
         #region Constructors
         public LoginViewModel()
         {
             this.IsRemembered = true;
+            this.IsEnabled = true;
         }
         #endregion
 
         #region Commands
         public ICommand LoginCommand
-        { 
+        {
             get
             {
                 return new RelayCommand(Login);
@@ -57,6 +106,7 @@
                     "Error",
                     "Te recomendamos ingresar un Email válido",
                     "Acepto");
+                return;
             }
 
             if (string.IsNullOrEmpty(this.Password))
@@ -65,7 +115,33 @@
                     "Error",
                     "Te recomendamos ingresar una contraseña válida",
                     "Acepto");
+                return;
             }
+
+            this.IsRunning = true;
+            this.IsEnabled = false;
+
+            if (this.Email != "temochas_lost@hotmail.com" || this.Password != "wwssdadabass1")
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "El Email o contraseña es incorrecto",
+                    "Acepto");
+                this.Password = string.Empty;
+                return;
+            }
+
+            this.IsRunning = false;
+            this.IsEnabled = true;
+
+            await Application.Current.MainPage.DisplayAlert(
+                "Eso Mijo",
+                "el pequeño delfin que hacia aaaaah aaah aah ",
+                "Acepto");
+            return;
+
         }
         #endregion
     }
